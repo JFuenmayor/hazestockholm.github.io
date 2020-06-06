df8b2e744c2d1d7ef66dd19a03c607f8a7d0d332
jfuenmayor <jorge.fuenmayort@gmail.com>
2020-06-05 23:23:32 -0500

    first commit

---
+++ precache-manifest.2efb64d9dcd762ce38af56dfa664aa6a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db1e1778288482eca920e0cb7f81faa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8582aca88cde291e4f0",
+    "url": "/static/css/main.1b8ac635.chunk.css"
+  },
+  {
+    "revision": "afe668b899f7e69052e1",
+    "url": "/static/js/2.df184928.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.df184928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8582aca88cde291e4f0",
+    "url": "/static/js/main.0cb02c4e.chunk.js"
+  },
+  {
+    "revision": "a4978f41b735423116c4",
+    "url": "/static/js/runtime-main.f385538a.js"
+  },
+  {
+    "revision": "0390549854957c829de13ea8120c986a",
+    "url": "/static/media/adagio-for-strings.03905498.mp3"
+  },
+  {
+    "revision": "8572f2aa2f8d1c00f243a00c292b6f21",
+    "url": "/static/media/fog1.8572f2aa.png"
+  },
+  {
+    "revision": "98e53ce7c711768e7a950b4090479eff",
+    "url": "/static/media/fog2.98e53ce7.png"
+  },
+  {
+    "revision": "66dafb8b464534abd608ff70006785f3",
+    "url": "/static/media/she_bad.66dafb8b.png"
+  },
+  {
+    "revision": "834ed812e67ad5f27418d53ae3398a22",
+    "url": "/static/media/she_good.834ed812.png"
+  },
+  {
+    "revision": "ed1872ef6d23cf7e10049b79297921a9",
+    "url": "/static/media/smoke.ed1872ef.mp4"
+  }
+]);
